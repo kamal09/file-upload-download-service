c91f7cf012c0d124abe03f7a66244b78b5e25cd2
Md. Kamal Hossain <sonycsecu2@gmail.com>
2022-03-21 07:22:48 +0600

    file upload

---
+++ file-upload-download-service/file-upload-download-service/src/main/java/com/kamal/fileuploaddownloadservice/FileUploadDownloadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kamal.fileuploaddownloadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadDownloadServiceApplication.class, args);
+	}
+
+}
---
+++ file-upload-download-service/file-upload-download-service/src/test/java/com/kamal/fileuploaddownloadservice/FileUploadDownloadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamal.fileuploaddownloadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadDownloadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
